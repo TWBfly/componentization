d79dc2a2b71a1aa1639916977903458bdc0027a4
TWBfly <1063351761@qq.com>
2019-11-05 11:10:06 +0800

    组件化入门

---
+++ library/src/main/java/win/tang/library/debug/Library_Debug_MainActivity.kt
@@ -0,0 +1,13 @@
+package win.tang.library.debug
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Tang on 2019/11/5
+ */
+class Library_Debug_MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ order/src/main/java/win/tang/order/Order_MainActivity.kt
@@ -0,0 +1,12 @@
+package win.tang.order
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Order_MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order__main)
+    }
+}
